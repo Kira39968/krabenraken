e25cdb4e659b856d80554c63b06bf023dc1b47a0
Lorraine7118 <d.mischenko.niko@gmail.com>
2016-09-20 02:05:31 +0300

    парап

--- Lab1/Programm2.cs
+++ Lab1/Programm2.cs
@@ -8,7 +8,7 @@ namespace Lab1
 {
     class Programm2
     {
-        //рлплпдлр лр длрjkllikfbн оен ое оп dsfgjkl;
+        //рлплпдлр лр длрjkllikfbн оен ое оп dsfgjkl; Кира пидор
         //рлплпдлр лр длрфывфывыфвфывфыв
         //new change
         //55555
