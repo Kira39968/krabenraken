31ac67ee9df4f1c5787202890beb1924ca3844b0
Кирилл <kovkiralex@gmail.com>
2016-09-19 23:16:43 +0300

    proj

---
+++ Lab1/Paper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Paper
+    {
+        public string Name { get; set; }
+        public Person Author { get; set; }
+        public DateTime DateOfPublication { get; set; }
+
+        public Paper(string name, Person author, DateTime dateOfPublication)
+        {
+            this.Name = name;
+            this.Author = author;
+            this.DateOfPublication = dateOfPublication;
+        }
+
+        public Paper()
+        {
+            this.Name = "";
+            this.Author = new Person("unnamed", "unnamed", new DateTime(1900, 01, 01));
+            this.DateOfPublication = new DateTime(1900, 01, 01);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Name of article {0}, Autor {1}, Date of publication {2}", Name, Author, DateOfPublication);
+        }
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ResearchTeam DyatlovTeam = new ResearchTeam("Pereval", "Dyatlov Team", 6845312, TimeFrame.Year);
+            Console.WriteLine(DyatlovTeam.ToShortString());
+
+            Console.WriteLine();
+            Console.WriteLine(DyatlovTeam[TimeFrame.Year]);
+            Console.WriteLine(DyatlovTeam[TimeFrame.TwoYears]);
+
+            DyatlovTeam.ResearchPublications = new Paper[]{new Paper()};
+            Person Dyatlov = new Person("Igor", "Dyatlov", new DateTime(1936, 01, 13));
+            Paper p1 = new Paper("Taina", Dyatlov, new DateTime(1959, 02, 02));
+            Paper p2 = new Paper("Taina2", Dyatlov, new DateTime(1960, 02, 03));
+
+            DyatlovTeam.AddPapers(p1, p2);
+            Console.WriteLine(DyatlovTeam.ToShortString());
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
