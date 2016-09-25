bf60a743213435dd3def6bfa4370a9db81aa7c0f
Lorraine7118 <d.mischenko.niko@gmail.com>
2016-09-25 15:53:50 +0300

    lab 2

---
+++ Lab2/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Person
+    {
+        private string name;
+        private string sName;
+        private DateTime birth;
+
+        public Person()
+        {
+            DateTime _dt = new DateTime(0000,01,01);
+            this.name = "unnamed";
+            this.sName = "unnamed";
+            this.birth = _dt;
+        }
+
+        public Person(string name, string sName, DateTime birth)
+        {
+            this.name = name;
+            this.sName = sName;
+            this.birth = birth;
+        }
+
+        public string Name
+        { 
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public string SurName
+        {
+            get { return this.sName; }
+            set { this.sName = value; }
+        }
+
+        public DateTime DateOfBirth 
+        {
+            get { return this.birth; }
+            set { this.birth = value; }
+        }
+
+        public int YearOfBirth
+        {
+            get { return birth.Year; }
+            set { birth.AddYears(value - birth.Year); }
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + this.name + ". Surname: " + this.sName + ". Date of Birth: " + this.birth;
+        }
+
+        public virtual string ToShortString()
+        {
+            return "Name: " + this.name + ". Surname: " + this.sName;
+        }
+
+        
+
+    }
+}
---
+++ Lab2/ResearchTeam.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class ResearchTeam
+    {
+        private string researchTheme;
+        private string researchTeam;
+        private int researchRegisterCode;
+        private TimeFrame researchDuration;
+        private Paper[] researchPublications;
+
+        public ResearchTeam(string ResearchTheme, string ResearchTeam, int ResearchRegisterCode, TimeFrame ResearchDuration)
+        {
+            this.researchTheme = ResearchTheme;
+            this.researchTeam = ResearchTeam;
+            this.researchRegisterCode = ResearchRegisterCode;
+            this.researchDuration = ResearchDuration;
+        }
+
+        public ResearchTeam()
+        {
+            this.researchTheme = "new Theme";
+            this.researchTeam = "new Team";
+            this.researchRegisterCode = 0000000;
+            this.researchDuration = TimeFrame.Long;
+        }
+
+        public string ResearchTheme { get { return this.researchTheme; } set { this.researchTheme = value; } }
+        public string _ResearchTeam { get { return this.researchTeam; } set { this.researchTeam = value; } }
+        public int ResearchRegisterCode { get { return this.researchRegisterCode; } set { this.researchRegisterCode = value; } }
+        public TimeFrame ResearchDuration { get { return this.researchDuration; } set { this.researchDuration = value; } }
+        public Paper[] ResearchPublications { get { return this.researchPublications; } set { this.researchPublications = value; } }
+
+        public bool this[TimeFrame time]
+        {
+            get { return time == this.researchDuration ? true : false; }
+        }
+
+        public void AddPapers ( params Paper[] papers)
+        {
+            Paper[] temp = new Paper[this.ResearchPublications.Length + papers.Length];
+            temp = this.ResearchPublications.Concat(papers).ToArray();
+            this.ResearchPublications = temp;
+        }
+
+        public override string ToString()
+        {
+            return String.Format(" Research group {0};\n Theme of reserch {1};\n Group`s Register code {2};\n Research duration {3};\n Research publications {4}",
+                _ResearchTeam, ResearchTheme, ResearchRegisterCode, ResearchDuration, ResearchPublications.ToList());
+        }
+
+        public virtual string ToShortString()
+        {
+            return String.Format(" Research group {0};\n Theme of reserch {1};\n Group`s Register code {2};\n Research duration {3}",
+                _ResearchTeam, ResearchTheme, ResearchRegisterCode, ResearchDuration);
+        }
+    }
+}
