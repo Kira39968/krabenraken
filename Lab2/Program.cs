5adedd4bf9e0f35f7c625d6c15cec36bbbced308
Кирилл <kovkiralex@gmail.com>
2016-09-25 19:43:43 +0300

    228

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -10,17 +10,8 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
-            DateTime birth = new DateTime(2007,01,27);
-            Person Hui = new Person("Hui", "Pesda", birth);
-            Console.WriteLine(Hui.GetHashCode());
 
-            birth = new DateTime(2007, 01, 27);
-            Person Chmo = new Person("mmm", "Pesda", birth);
-            Console.WriteLine(Chmo.GetHashCode());
-            Console.WriteLine("1 " + Hui.GetHashCode());
-            Console.WriteLine("2 " + Hui.GetHashCode());
-            Console.WriteLine("3 " + Hui.GetHashCode());
-            Console.WriteLine("4 " + Hui.GetHashCode());
+
             Console.ReadKey();
         }
     }
